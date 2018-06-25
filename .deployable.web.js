f30240a695a0f16d2fb5f347d8df026eef6f8108
Steudnera C. Koch <wowcxy2008@126.com>
2018-06-25 18:28:40 +0800

    Create .deployable.web.js

---
+++ .deployable.web.js
@@ -0,0 +1,21 @@
+/**
+ * 部署配置
+ * @author Steudnera
+ */
+module.exports = {
+  cdn: {
+    domain: '',
+    bukect: '',
+    accessKey: '',
+    secretKey: '',
+  },
+  servers: ['blog.admin'],
+  portals: {
+    example: '',
+  },
+  output: {
+    manifest: "",
+    folder: "",
+  },
+  build: "build",
+}
