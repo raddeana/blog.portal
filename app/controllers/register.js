5e3398b7c8bc95e152ccbd6e42e689dfbf15357f
Philip <wowcxy2008@126.com>
2018-12-04 10:45:23 +0800

    Update register.js

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,4 +1,20 @@
 /**
- * 注册
- * @Steudnera
+ * 登录
+ * @author Philip
  */
+
+/**
+ * 登录
+ * @controller
+ */
+module.exports.register = function () {
+  
+}
+
+/**
+ * 获取识别码图片
+ * @controller
+ */
+module.exports.getVerifyCodeImg = function () {
+  
+}
