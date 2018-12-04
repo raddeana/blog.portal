2765855ee5b881774d9165a3c02edef6d33073f4
Philip <wowcxy2008@126.com>
2018-12-04 10:36:38 +0800

    Update forget-password.js

--- app/controllers/forget-password.js
+++ app/controllers/forget-password.js
@@ -1,1 +1,20 @@
+/**
+ * 忘记密码
+ * @author Philip
+ */
 
+/**
+ * 发送验证码
+ * @ctrl
+ */
+module.exports.sendVerifyCode = function () {
+
+}
+
+/**
+ * 重设密码
+ * @ctrl
+ */
+module.exports.resetPassword = function () {
+
+}
