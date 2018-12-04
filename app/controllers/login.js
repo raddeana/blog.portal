60f71a1ad713f49ac9ab73401ba5c41c9cd84c10
Philip <wowcxy2008@126.com>
2018-12-04 10:40:15 +0800

    Update login.js

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,1 +1,20 @@
+/**
+ * 登录
+ * @author Philip
+ */
 
+/**
+ * 登录
+ * @controller
+ */
+module.exports.login = function () {
+  
+}
+
+/**
+ * 获取识别码图片
+ * @controller
+ */
+module.exports.getVerifyCodeImg = function () {
+  
+}
